7377794a768eba4ee074aef0e5820f434413bec6
Ramil Safin <ramilsafnab1996@gmail.com>
2018-11-04 22:21:54 +0300

    Add custom messages package

---
+++ src/custom_message/src/RobotStatus.cpp
@@ -0,0 +1,62 @@
+#include <ros/ros.h>
+#include <string>
+
+#include "custom_message/RobotStatus.h"
+
+/* constants */
+static constexpr auto STATUS_PUBLISHING_RATE = 1;
+
+static constexpr auto HIGH_DURABILITY_LEVEL_SEC = 60;
+static constexpr auto AVERAGE_DURABILITY_LEVEL_SEC = 45;
+static constexpr auto LOW_DURABILITY_LEVEL_SEC = 30;
+
+int main(int argc, char ** argv) {
+    ros::init(argc, argv, "robot_status_node");
+    
+    ros::NodeHandle node;
+    ros::NodeHandle node_("~");
+
+    // read parameters
+    int robotId;
+    std::string durabilityLevel;  // high, average, low
+
+    node_.param("robot_id", robotId, -1);
+    node_.param("durability_level", durabilityLevel, std::string{"average"});
+
+    auto pub = node.advertise<custom_message::RobotStatus>("status", 5);
+
+    custom_message::RobotStatus statusMsg;
+    statusMsg.robot_id = robotId;
+
+    ros::Rate rate(STATUS_PUBLISHING_RATE);
+
+    ROS_INFO("Starting to pubish robot status messages");
+
+    auto startTime = ros::Time::now();
+
+    double secondsTillDeath;
+    
+    if (durabilityLevel == "high") {
+        secondsTillDeath = HIGH_DURABILITY_LEVEL_SEC;
+    } else if (durabilityLevel == "average") {
+        secondsTillDeath = AVERAGE_DURABILITY_LEVEL_SEC;
+    } else {
+        secondsTillDeath = LOW_DURABILITY_LEVEL_SEC;
+    }
+
+    while (ros::ok()) {
+      // update time and robot's status
+      statusMsg.header.stamp = ros::Time::now();
+      
+      if ((statusMsg.header.stamp - startTime).toSec() > secondsTillDeath) {
+        statusMsg.is_ok = false;
+      } else {
+        statusMsg.is_ok = true;
+      }
+
+      pub.publish(statusMsg);
+      rate.sleep();
+    }
+
+    return 0;
+}
